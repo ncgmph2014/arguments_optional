331814e29fe0babfac7713650df60e63fec9137a
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-17 14:49:40 -0500

    trying to fix because it was not working

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function addTogether() {
-  let args = [...arguments];
+  let args = arguments;
+  let sum = 0;
 
-  //return false;
+  for (let i = 0; i < args.length; i++) {
+    sum += args[i];
+  }
+  return sum;
 }
 
 console.log(addTogether(2, 3));
@@ -10,3 +14,4 @@ console.log(addTogether(2)(3) + " should return 5.");
 console.log(addTogether("http://bit.ly/IqT6zt") + " should return undefined.");
 console.log(addTogether(2, "3") + " should return undefined.");
 console.log(addTogether(2)([3]) + " should return undefined.");
+console.log("Something is happening here");
